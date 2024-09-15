7947fd63133a0049e3e129d99fec1042eef26bd9
George Akram <118694096+GEOREHNUEDDFF@users.noreply.github.com>
2024-09-15 13:28:44 +0300

    Add files via upload

---
+++ src/components/widget/Widget.jsx
@@ -0,0 +1,102 @@
+import "./widget.scss";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import Person2 from "@mui/icons-material/PeopleAltTwoTone";
+
+
+const Widget = ({ type }) => {
+  let data;
+
+  //temporary
+  const amount = 100;
+  const diff = 20;
+
+  switch (type) {
+    case "user":
+      data = {
+        title: "People",
+        isMoney: false,
+        link: "See all users",
+        icon: (
+          <PersonOutlinedIcon
+            className="icon"
+            style={{
+              color: "crimson",
+              backgroundColor: "rgba(255, 0, 0, 0.2)",
+            }}
+          />
+        ),
+      };
+      break;
+    case "order":
+      data = {
+        title: "Families",
+        isMoney: false,
+        link: "View all orders",
+        icon: (
+          <Person2
+            className="icon"
+            style={{
+              backgroundColor: "rgba(218, 165, 32, 0.2)",
+              color: "goldenrod",
+            }}
+          />
+        ),
+      };
+      break;
+    case "earning":
+      data = {
+        title: "Admins",
+        isAdmin: true,
+        link: "View admin page",
+        icon: (
+          <PersonOutlinedIcon
+            className="icon"
+            style={{ backgroundColor: "rgba(0, 128, 0, 0.2)", color: "green" }}
+          />
+        ),
+      };
+      break;
+    case "balance":
+      data = {
+        title: "Total partners",
+        isPeople: true,
+        link: "See details",
+        icon: (
+          <PersonOutlinedIcon
+            className="icon"
+            style={{
+              backgroundColor: "rgba(128, 0, 128, 0.2)",
+              color: "purple",
+            }}
+          />
+        ),
+      };
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <div className="widget">
+      <div className="left">
+        <span className="title">{data.title}</span>
+        <span className="counter">
+          {data.isPeople  && "||" } {amount}
+        </span>
+        <span className="link">{data.link}</span>
+        
+      </div>
+    
+      <div className="right">
+        <div className="percentage positive">
+          <KeyboardArrowUpIcon />
+          {diff} %
+        </div>
+        {data.icon}
+      </div>
+    </div>
+  );
+};
+
+export default Widget;
